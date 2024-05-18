2504739a1245e0cc5890d79100591ae9aa984440
Flame260 <162753205+Flame260@users.noreply.github.com>
2024-05-18 19:05:16 -0400

    player and camera

---
+++ final project/Assets/SCRIPTS/camera behavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerabehavior : MonoBehaviour
+{
+    public Vector3 CamOffset = new Vector3(2f,4f,-6f);
+
+    private Transform _target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+      this.transform.position = _target.TransformPoint(CamOffset);
+
+      this.transform.LookAt(_target);
+    }
+}
